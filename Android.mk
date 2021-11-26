36c49f9f5fc4754b6d9282b17aaa602207c0d2c8
Katharine Chui <kwchuiaa@connect.ust.hk>
2021-11-26 12:10:55 +0800

    treble changes

--- Android.mk
+++ Android.mk
@@ -22,3 +22,28 @@ ifneq ($(filter herolte hero2lte, $(TARGET_DEVICE)),)
   $(foreach mk,$(subdir_makefiles),$(info including $(mk) ...)$(eval include $(mk)))
 
 endif
+
+# install symlinks for vendor libs
+include $(CLEAR_VARS)
+
+ifneq ($(filter herolte, $(TARGET_DEVICE)),)
+VENDOR_OUT_PATH := out/target/product/herolte/vendor
+endif
+
+ifneq ($(filter hero2lte, $(TARGET_DEVICE)),)
+VENDOR_OUT_PATH := out/target/product/hero2lte/vendor
+endif
+
+LOCAL_MODULE := symlinks-universal8890-common
+LOCAL_SRC_FILES := symlinks
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH := $(VENDOR_OUT_PATH)
+LOCAL_MODULE_TAGS := optional
+LOCAL_POST_INSTALL_CMD := echo creating symlinks to $(VENDOR_OUT_PATH) ;\
+  ln -sf libgui_vendor.so $(VENDOR_OUT_PATH)/lib/libgui.so ;\
+  ln -sf libgui_vendor.so $(VENDOR_OUT_PATH)/lib64/libgui.so ;\
+  ln -sf libcamera_client_vendor.so $(VENDOR_OUT_PATH)/lib/libcamera_client.so ;\
+  ln -sf libcamera_client_vendor.so $(VENDOR_OUT_PATH)/lib64/libcamera_client.so ;\
+  ln -sf libprotobuf-cpp-lite-3.9.1.so $(VENDOR_OUT_PATH)/lib/libprotobuf-cpp-lite.so ;\
+  ln -sf libprotobuf-cpp-lite-3.9.1.so $(VENDOR_OUT_PATH)/lib64/libprotobuf-cpp-lite.so
+include $(BUILD_PREBUILT)
