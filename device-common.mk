e7d5028561a354ef350f5f35852df032f5910040
Katharine <kwchuiaa@connect.ust.hk>
2021-12-08 19:51:51 +0800

    batterylifeextender is not ready yet

--- device-common.mk
+++ device-common.mk
@@ -69,8 +69,8 @@ PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/usb_audio_policy_configuration.xml
 
 # BatteryLifeExtender
-PRODUCT_PACKAGES += \
-    vendor.lineage.batterylifeextender@1.0-service.samsung
+#PRODUCT_PACKAGES += \
+#    vendor.lineage.batterylifeextender@1.0-service.samsung
 
 # Bluetooth
 PRODUCT_PACKAGES += \
